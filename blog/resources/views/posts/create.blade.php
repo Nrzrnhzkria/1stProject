2899d5c64ca8df74e96d11b3e4f27be84f20abc6
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 15:40:21 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -3,33 +3,17 @@
 @section('content')
     <h1>Our Plan</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-    <section class="pricing py-5">
-        <div class="container">
-          <div class="row">
-            <!-- Free Tier -->
-            <div class="col-lg-4">
-              <div class="card mb-5 mb-lg-0">
-                <div class="card-body">
-                  <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
-                  <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
-                  <hr>
-                  <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                    <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
-                    <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
-                    <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
-                    <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
-                  </ul>
-                  <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </section>
+    <div class="columns">
+        <ul class="price">
+          <li class="header">Basic</li>
+          <li class="grey">$ 9.99 / year</li>
+          <li>10GB Storage</li>
+          <li>10 Emails</li>
+          <li>10 Domains</li>
+          <li>1GB Bandwidth</li>
+          <li class="grey"><a href="#" class="button">Sign Up</a></li>
+        </ul>
+    </div>
         
         
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
