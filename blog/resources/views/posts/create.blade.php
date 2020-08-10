1e414844306021f9454024dfd9eff433a5ca86ec
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 15:56:51 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -3,18 +3,27 @@
 @section('content')
     <h1>Create Post</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        <div class="form-group">
-            <div class="columns">
-                <ul class="price">
-                  <li class="header">Basic</li>
-                  <li class="grey">$ 9.99 / year</li>
-                  <li>10GB Storage</li>
-                  <li>10 Emails</li>
-                  <li>10 Domains</li>
-                  <li>1GB Bandwidth</li>
-                  <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                </ul>
-              </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Dashboard</div>
+                          <div class="panel-body">
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">Basic</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="form-group">
             {{Form::label('body', 'Body')}}
