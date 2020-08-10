3290cdddfe5e4bcfc690e6c81ee9c4a122a259e4
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 16:18:48 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -6,8 +6,19 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Roket Pemasaran Momentum (RPM)</div>
-                          <div class="panel-body">
+                        <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
+                            <div class="panel-body">
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">General</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
 
                             <div class="columns">
                                 <ul class="price">
@@ -23,7 +34,7 @@
 
                             <div class="columns">
                                 <ul class="price">
-                                <li class="header">Gold</li>
+                                <li class="header">General</li>
                                 <li class="grey">$ 9.99 / year</li>
                                 <li>10GB Storage</li>
                                 <li>10 Emails</li>
@@ -32,19 +43,7 @@
                                 <li class="grey"><a href="#" class="button">Sign Up</a></li>
                                 </ul>
                             </div>
-
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">Diamond</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
                             </div>
-                          </div>
 
                         </div>
                     </div>
