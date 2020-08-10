b1c38d77b8652bf780b3888517f9b38201b1eb91
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 16:12:42 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -4,7 +4,6 @@
     <h1>Our Plan</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
-        <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
@@ -46,58 +45,8 @@
                                 <li class="grey"><a href="#" class="button">Sign Up</a></li>
                                 </ul>
                             </div>
-
                           </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
 
-        <div class="form-group">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
-                              <div class="panel-body">
-                                <div class="columns">
-                                    <ul class="price">
-                                    <li class="header">General</li>
-                                    <li class="grey">$ 9.99 / year</li>
-                                    <li>10GB Storage</li>
-                                    <li>10 Emails</li>
-                                    <li>10 Domains</li>
-                                    <li>1GB Bandwidth</li>
-                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="columns">
-                                    <ul class="price">
-                                    <li class="header">General</li>
-                                    <li class="grey">$ 9.99 / year</li>
-                                    <li>10GB Storage</li>
-                                    <li>10 Emails</li>
-                                    <li>10 Domains</li>
-                                    <li>1GB Bandwidth</li>
-                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="columns">
-                                    <ul class="price">
-                                    <li class="header">General</li>
-                                    <li class="grey">$ 9.99 / year</li>
-                                    <li>10GB Storage</li>
-                                    <li>10 Emails</li>
-                                    <li>10 Domains</li>
-                                    <li>1GB Bandwidth</li>
-                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                    </ul>
-                                </div>
-
-                              </div>
                         </div>
                     </div>
                 </div>
@@ -105,7 +54,55 @@
         </div>
 
         <div class="form-group">
-            <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
+                            <div class="panel-body">
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">General</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">General</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">General</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <div class="panel panel-default">
@@ -147,10 +144,10 @@
                                     </ul>
                                 </div>
                               </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
