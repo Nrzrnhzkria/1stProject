f6aa5434bbd9d3cfa0a5fb420054d4944f4ca134
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 14:50:16 +0800

    css

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Styles -->
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
-    <link href="{{ css/styles.css }}" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
