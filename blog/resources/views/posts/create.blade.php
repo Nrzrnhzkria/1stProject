186c832de7aac2aefebe2c6f628ec2fc4f3851d4
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 16:07:34 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create Post</h1>
+    <h1>Our Plan</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        <div class="form-group">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Dashboard</div>
+                        <div class="panel-heading">Roket Pemasaran Momentum (RPM)</div>
                           <div class="panel-body">
+
                             <div class="columns">
                                 <ul class="price">
-                                <li class="header">Basic</li>
+                                <li class="header">General</li>
                                 <li class="grey">$ 9.99 / year</li>
                                 <li>10GB Storage</li>
                                 <li>10 Emails</li>
@@ -20,17 +22,135 @@
                                 <li class="grey"><a href="#" class="button">Sign Up</a></li>
                                 </ul>
                             </div>
+
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">Gold</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="columns">
+                                <ul class="price">
+                                <li class="header">Diamond</li>
+                                <li class="grey">$ 9.99 / year</li>
+                                <li>10GB Storage</li>
+                                <li>10 Emails</li>
+                                <li>10 Domains</li>
+                                <li>1GB Bandwidth</li>
+                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                </ul>
+                            </div>
+
                           </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="form-group">
-            {{Form::label('body', 'Body')}}
-            {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
         </div>
+
         <div class="form-group">
-            {{Form::file('cover_image')}}
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
+                              <div class="panel-body">
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+
+                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Intensif Momentum Pemasaran Internet (IMPI)</div>
+                              <div class="panel-body">
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="columns">
+                                    <ul class="price">
+                                    <li class="header">General</li>
+                                    <li class="grey">$ 9.99 / year</li>
+                                    <li>10GB Storage</li>
+                                    <li>10 Emails</li>
+                                    <li>10 Domains</li>
+                                    <li>1GB Bandwidth</li>
+                                    <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                                    </ul>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
