5ea33720ac1e49e4bcc91813d8fb6a6e9a175de0
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 15:43:49 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -3,17 +3,23 @@
 @section('content')
     <h1>Our Plan</h1>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-    <div class="columns">
-        <ul class="price">
-          <li class="header">Basic</li>
-          <li class="grey">$ 9.99 / year</li>
-          <li>10GB Storage</li>
-          <li>10 Emails</li>
-          <li>10 Domains</li>
-          <li>1GB Bandwidth</li>
-          <li class="grey"><a href="#" class="button">Sign Up</a></li>
-        </ul>
+    <div class="container">
+          <div class="row">
+            <!-- General Tier -->
+            <div class="columns">
+                <ul class="price">
+                  <li class="header">General</li>
+                  <li class="grey">$ 9.99 / year</li>
+                  <li>10GB Storage</li>
+                  <li>10 Emails</li>
+                  <li>10 Domains</li>
+                  <li>1GB Bandwidth</li>
+                  <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                </ul>
+              </div>
+          </div>
     </div>
+
         
         
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
