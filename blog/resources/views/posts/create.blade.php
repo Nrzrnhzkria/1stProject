099285a218a4ddb3712cbba98323b52a8969615c
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 17:22:39 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
+                        <div class="panel-heading" style="background-color: crimson">Roket Pemasaran Momentum (RPM)</div>
                             <div class="panel-body">
                             <div class="columns">
                                 <ul class="price">
