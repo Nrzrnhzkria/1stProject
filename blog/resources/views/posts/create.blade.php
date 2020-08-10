f2bde59eb6b75b6555803cd659f424ba7adfe127
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-10 17:28:55 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
 
         <div class="form-group">
             <div class="row">
-                <div class="col-md-12 ">
+                <div class="col-md-10 ">
                     <div class="panel panel-default">
                         <div class="panel-heading">Roket Pemasaran Momentum (RPM)</div>
                             <div class="panel-body">
