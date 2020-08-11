fc5ce2b00f2fdc5066b60fabf5a9acbd440edb0a
Nrzrnhzkria <zarina4.11@gmail.com>
2020-08-11 11:18:47 +0800

    plan

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -3,103 +3,56 @@
 @section('content')
     <h1>Our Plan</h1>
 
-        <div class="form-group">
-            <div class="row">
-                <div class="col-md-10 col-md-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Roket Pemasaran Momentum (RPM)</div>
-                            <div class="panel-body">
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
+        
 
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
-
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
-                            </div>
-
+    <div class="form-group">
+        <div class="row">
+            <div class="col-md-10 col-md-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Roket Pemasaran Momentum (RPM)</div>
+                        <div class="panel-body">
+                        <div class="columns">
+                            <ul class="price">
+                            <li class="header">General</li>
+                            <li class="grey">$ 9.99 / year</li>
+                            <li>10GB Storage</li>
+                            <li>10 Emails</li>
+                            <li>10 Domains</li>
+                            <li>1GB Bandwidth</li>
+                            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                            </ul>
                         </div>
+
+                        <div class="columns">
+                            <ul class="price">
+                            <li class="header">General</li>
+                            <li class="grey">$ 9.99 / year</li>
+                            <li>10GB Storage</li>
+                            <li>10 Emails</li>
+                            <li>10 Domains</li>
+                            <li>1GB Bandwidth</li>
+                            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="columns">
+                            <ul class="price">
+                            <li class="header">General</li>
+                            <li class="grey">$ 9.99 / year</li>
+                            <li>10GB Storage</li>
+                            <li>10 Emails</li>
+                            <li>10 Domains</li>
+                            <li>1GB Bandwidth</li>
+                            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+                            </ul>
+                        </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="form-group">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Advance Roket Bisnes (ARB)</div>
-                            <div class="panel-body">
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
-
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
-
-                            <div class="columns">
-                                <ul class="price">
-                                <li class="header">General</li>
-                                <li class="grey">$ 9.99 / year</li>
-                                <li>10GB Storage</li>
-                                <li>10 Emails</li>
-                                <li>10 Domains</li>
-                                <li>1GB Bandwidth</li>
-                                <li class="grey"><a href="#" class="button">Sign Up</a></li>
-                                </ul>
-                            </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
 
         <div class="form-group">
                 <div class="row">
